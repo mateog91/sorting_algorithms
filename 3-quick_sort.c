f02d41decae2a8d413cde9a82b4f068b53d2d3e1
Mateo <mateog91@gmail.com>
2021-10-24 20:23:25 -0500

    ADD: BETTY

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,34 +10,34 @@
  */
 int partition(int *array, int lo, int hi, size_t size)
 {
-    int pivot = array[hi];
-    int i = lo;
-    int j;
-    int tmp;
-    int tmp2;
+	int pivot = array[hi];
+	int i = lo;
+	int j;
+	int tmp;
+	int tmp2;
 
-    for (j = lo; j < hi; j++)
-    {
-        if (array[j] < pivot)
-        {
-            if (array[i] != array[j])
-            {
-                tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-                print_array(array, size);
-            }
-            i++;
-        }
-    }
-    if (array[i] != array[hi])
-    {
-        tmp2 = array[i];
-        array[i] = array[hi];
-        array[hi] = tmp2;
-        print_array(array, size);
-    }
-    return (i);
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (array[i] != array[j])
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (array[i] != array[hi])
+	{
+		tmp2 = array[i];
+		array[i] = array[hi];
+		array[hi] = tmp2;
+		print_array(array, size);
+	}
+	return (i);
 }
 /**
  * sort - making recursion for quick sort
@@ -48,14 +48,14 @@ int partition(int *array, int lo, int hi, size_t size)
  */
 void sort(int *array, int lo, int hi, size_t size)
 {
-    int p;
+	int p;
 
-    if (lo < hi)
-    {
-        p = partition(array, lo, hi, size);
-        sort(array, lo, p - 1, size);
-        sort(array, p + 1, hi, size);
-    }
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, size);
+		sort(array, lo, p - 1, size);
+		sort(array, p + 1, hi, size);
+	}
 }
 /**
  * quick_sort - implement quick_sort algorithm
@@ -64,5 +64,5 @@ void sort(int *array, int lo, int hi, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    sort(array, 0, (int)size - 1, size);
+	sort(array, 0, (int)size - 1, size);
 }
