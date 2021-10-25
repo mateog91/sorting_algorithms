cefedd2dd40a2ce23cf975b61b2160d31d574ecd
Mateo <mateog91@gmail.com>
2021-10-25 15:06:05 -0500

    ADD: file 100 and UPD: sort.h with functions inside

---
+++ 100-shell_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+/**
+ * _pow - returns the value of x raised to the power of y
+ * @x: input value of exponential base
+ * @y: input value for exponent
+ *
+ * Return: Interger of @x to the power of @y
+ */
+int _pow(int x, int y)
+{
+	/* Base Case */
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	/* Recursive Case */
+	return (x * _pow(x, (y - 1)));
+}
+/**
+ * shell_sort -  function that sorts an array of integers in ascending order
+ *  using the Shell sort algorithm, using the Knuth sequence
+ *
+ * @array: Pointer to the input array
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+
+	size_t i, j, gap, n;
+
+	n = size / 3;
+	while (1)
+	{
+		n--;
+		if (n < 1)
+			break;
+
+		gap = (_pow(3, n) - 1) / 2;
+		if (gap > size)
+		{
+
+			continue;
+		}
+
+		for (i = gap; i < size; i++)
+		{
+			int temp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -31,4 +31,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void sort(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
+/* 100-shell_sort.c file */
+void shell_sort(int *array, size_t size);
+int _pow(int x, int y);
 #endif /* sort.h */
