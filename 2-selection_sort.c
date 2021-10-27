284bde0b3b05e28d9477e3ff7208e11f37233fe8
Mateo <mateog91@gmail.com>
2021-10-26 19:39:45 -0500

    BETTY

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,30 +9,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, jMin;
-    int temp;
+	size_t i, j, jMin;
+	int temp;
 
-    /* Go through list */
-    for (i = 0; i < size; i++)
-    {
-        jMin = i;
-        /*
-        printf("%li\n", i);
-        */
-        /* Search for the minimum */
-        for (j = i + 1; j < size; j++)
-        {
-            /* store minimun index*/
-            if (array[j] < array[jMin])
-                jMin = j;
-        }
-        /* Swap the current index with the minimum */
-        if (jMin != i)
-        {
-            temp = array[jMin];
-            array[jMin] = array[i];
-            array[i] = temp;
-            print_array(array, size);
-        }
-    }
+	/* Go through list */
+	for (i = 0; i < size; i++)
+	{
+		jMin = i;
+
+		/* Search for the minimum */
+		for (j = i + 1; j < size; j++)
+		{
+			/* store minimun index*/
+			if (array[j] < array[jMin])
+				jMin = j;
+		}
+		/* Swap the current index with the minimum */
+		if (jMin != i)
+		{
+			temp = array[jMin];
+			array[jMin] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
 }
