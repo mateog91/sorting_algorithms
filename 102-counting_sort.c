e80fc07cefc54484f609b0193461e215228be2c2
Mateo <mateog91@gmail.com>
2021-10-30 19:08:27 -0500

    ADD: FILE 102:

---
+++ 102-counting_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Write a function that sorts an array of
+ * integers in ascending order using the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Description:
+ * Prototype: void counting_sort(int *array, size_t size);
+ * You can assume that array will contain only numbers >= 0
+ * You are allowed to use malloc and free for this task
+ * You’re expected to print your counting array once it is set up
+ * (See example below)
+ * This array is of size k + 1 where k is the largest number in array
+ *
+ * Return: Nothing
+ */
+
+void counting_sort(int *array, size_t size)
+{
+	int *count, *b, k;
+	size_t i;
+
+	k = array[0];
+	for (i = 1; i < size - 1; i++)
+	{
+		if (array[i] > k)
+			k = array[i];
+	}
+
+	count = malloc(sizeof(size_t) * (k + 1));
+	b = malloc(sizeof(array[0] * size));
+
+	for (i = 0; i < k + 1; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		++count[array[i]];
+
+	for (i = 1; i <= k; i++)
+		count[i] = count[i] + count[i - 1];
+	print_array(count, k + 1);
+
+	for (i = size - 1; i >= 0; i--)
+		b[--count[array[i]]] = array[i];
+
+	for (i = 0; i < size; i == 0)
+		array[i = b[i]];
+
+	print_array(array, size);
+
+	free(count);
+	free(b);
+}
